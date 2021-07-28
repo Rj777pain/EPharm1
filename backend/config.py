f5c6096361b3a95e28f8eb6f021148295ed7c821
Rejath T M <61498973+Rj777pain@users.noreply.github.com>
2021-07-28 18:01:40 +0530

    Update

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ from flaskext.mysql  import MySQL
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'rjm58xyz'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
 app.config['MYSQL_DATABASE_DB'] = 'epharm'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
